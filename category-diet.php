ba6d9435191972759ea2146ca9fb2bdcbfea7ce1
shigeshibu44 <shigeshibu44@gmail.com>
2017-03-15 21:33:32 +0900

    Add latest files

--- category-diet.php
+++ category-diet.php
@@ -11,40 +11,43 @@ get_header(); ?>
 
 	<main id="primary" class="content-area" role="main">
 
+
+							<?php
+
+								/*
+								 * Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'template-parts/diet-custom-header', 'blocks' );
+							?>
+
 		<div id="posts" class="posts">
 
-			<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<?php
+					get_the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+				<iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/140-TYOAhLd4MAEOF7wEb_LzKjC3mKArhG2jLXPyJ0qw/pubchart?oid=1167655288&amp;format=image"></iframe>
+			</header><!-- .page-header -->
 
-				<header class="page-header">
-					<?php
-						get_the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-					?>
-					<iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/140-TYOAhLd4MAEOF7wEb_LzKjC3mKArhG2jLXPyJ0qw/pubchart?oid=1167655288&amp;format=image"></iframe>
-				</header><!-- .page-header -->
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php
 
-					<?php
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'blocks' );
+				?>
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', 'blocks' );
-					?>
+			<?php endwhile; ?>
 
-				<?php endwhile; ?>
-
-				<?php the_posts_navigation(); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-			<?php endif; ?>
+			<?php the_posts_navigation(); ?>
 
 		</div><!-- .posts -->
 
